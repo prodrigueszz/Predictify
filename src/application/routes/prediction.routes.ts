e929796e58afde3bdc5ac889101721607957966e
Tiago <prodrigueszz271@gmail.com>
2025-04-25 19:33:03 -0300

    Prediction routes now implemented

--- src/application/routes/prediction.routes.ts
+++ src/application/routes/prediction.routes.ts
@@ -3,6 +3,9 @@ import { PredictionController } from '../controllers/prediction.controller';
 
 const PredictionRouter = Router();
 
-PredictionRouter.post("/prediction", PredictionController.createHandler);
+PredictionRouter.post("/predictions", PredictionController.createHandler);
+PredictionRouter.get("/predictions/:id", PredictionController.getByIdHandler);
+PredictionRouter.get("/predictions", PredictionController.getAllHandler);
+PredictionRouter.delete("/predictions/:id", PredictionController.deleteHandler);
 
 export default PredictionRouter;
